17b852f5dd437881055594022b2adc7c244ceab6
jskim <juseog0373@naver.com>
2024-01-16 03:44:22 +0900

    20240116 여러가지 파라미터 조회하기

--- src/main/java/hello/servlet/basic/request/RequestParamServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -14,8 +14,24 @@ public class RequestParamServlet extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("RequestParamServlet.service");
+        System.out.println("[전체 파라미터 조회] - start");
+        req.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> System.out.println("paramName = " + req.getParameter(paramName)));
+        System.out.println("[전체 파라미터 조회] - end");
+        System.out.println();
+
+        System.out.println("[단일 파라미터 조회] - start");
         String username = req.getParameter("username");
+        String age = req.getParameter("age");
         System.out.println("username = " + username);
+        System.out.println("age = " + age);
+        System.out.println("[단일 파라미터 조회] - end");
+
+        System.out.println("[중복 파라미터 조회] - start");
+        String[] usernames = req.getParameterValues("username");
+        for (String name : usernames) {
+            System.out.println("username = " + name);
+        }
+        System.out.println("[중복 파라미터 조회] - end");
     }
 }
