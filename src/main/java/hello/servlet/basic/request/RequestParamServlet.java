6ecf452bb0652f64548ba4b9856eb9c47fcdfb34
jskim <juseog0373@naver.com>
2024-01-16 01:39:50 +0900

    20240116 http get query param 분석하기

---
+++ src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -0,0 +1,21 @@
+package hello.servlet.basic.request;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jdk.jfr.Frequency;
+
+import java.io.IOException;
+
+@WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
+public class RequestParamServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("RequestParamServlet.service");
+        String username = req.getParameter("username");
+        System.out.println("username = " + username);
+    }
+}
