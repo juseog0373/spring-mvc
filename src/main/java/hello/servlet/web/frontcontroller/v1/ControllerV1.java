f6155ee371725e0505755ebeff9e888b07bfc306
jskim <juseog0373@naver.com>
2024-01-18 16:51:48 +0900

    frontController-V2 적용

--- src/main/java/hello/servlet/web/frontcontroller/v1/ControllerV1.java
+++ src/main/java/hello/servlet/web/frontcontroller/v1/ControllerV1.java
@@ -1,7 +1,6 @@
 package hello.servlet.web.frontcontroller.v1;
 
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
